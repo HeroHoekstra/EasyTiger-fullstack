abe40d5b39bc03f69a90bdf6a365ab4834401858
Hero Hoekstra <2172289@talnet.nl>
2023-06-20 11:07:23 +0200

    Worked on light and dark theme

--- js/theme.js
+++ js/theme.js
@@ -2,18 +2,18 @@ const styles = document.documentElement.style;
 
 // Light theme
 function set_light() {
-    styles.setProperty('--text', '#171a14');
-    styles.setProperty('--background', '#edf0ea');
-    styles.setProperty('--primary-button', '#c6becf');
-    styles.setProperty('--secondary-button', '#ffffff');
-    styles.setProperty('--accent', '#9284a4');
+    styles.setProperty('--text', '#000');
+    styles.setProperty('--background', '#fff');
+    styles.setProperty('--primary-color', '#8fb4ff');
+    styles.setProperty('--secondary-color', '#ebf1ff');
+    styles.setProperty('--accent', '#ff8f94');
 }
 
 // Dark theme
 function set_dark() {
     styles.setProperty('--text', '#e3ebed');
     styles.setProperty('--background', '#141d1f');
-    styles.setProperty('--primary-button', '#56648a');
-    styles.setProperty('--secondary-button', '#1f1b2c');
+    styles.setProperty('--primary-color', '#56648a');
+    styles.setProperty('--secondary-color', '#1f1b2c');
     styles.setProperty('--accent', '#62568a');
 }
