6f775ccff30f5bfe9edf936ed1b2420497503699
Hero Hoekstra <2172289@talnet.nl>
2023-06-27 11:16:58 +0200

    work

--- admin/response.php
+++ admin/response.php
@@ -1,11 +1,43 @@
 <?php
 include '../connect.php';
 
+function band_id($pdo) {
+    $band_id = hexdec(uniqid());
+
+    $query = $pdo->prepare("SELECT `band_id` FROM `band` WHERE `band_id` = :band_id");
+    $query->bindparam(":band_id", $band_id);
+    $query->execute();
+
+    $result = $query->fetchAll();
+
+    if ($result != NULL) {
+        band_id();
+    } else {
+        return $band_id;
+    }
+}
+
+print_r($_POST);
+
 try {
-    $query = $pdo->prepare(INSERT INTO band 
-        (Band_id, Naam, Genre, Herkomst, Omschrijving)
-        VALUES ())
+    $band_id = band_id($pdo);
+    $name = $_POST['name'];
+    $genre = $_POST['genre'];
+    $origin = $_POST['origin'];
+    $desc = $_POST['desc'];
+
+    $query = $pdo->prepare("INSERT INTO `band` 
+        (`Band_id`, `Naam`, `Genre`, `Herkomst`, `Omschrijving`)
+        VALUES (:band_id, :name, :genre, :origin, :desc)");
+    $query->bindparam(":band_id", $band_id);
+    $query->bindparam(":name", $name);
+    $query->bindparam(":genre", $genre);
+    $query->bindparam(":origin", $origin);
+    $query->bindparam(":desc", $desc);
+    $query->execute();
 } catch (Exception $e) {
     print("Error!: <br>" . $e->getMessage() . "<br>");
 }
+
+
 ?>
