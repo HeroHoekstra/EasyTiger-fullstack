fc35ce18eca1a68443a28f1c410947b56f736abb
Hero Hoekstra <2172289@talnet.nl>
2023-09-06 21:40:09 +0200

    finished-ish adding / editing bands
    You can now add and edit bands. but the the editing still needs the php to actually edit :)

--- admin/add_people.js
+++ admin/add_people.js
@@ -15,46 +15,61 @@ for (let i = 0; i < buttons.length; i++) {
 
 // Open add / edit people menu
 // There should be an equal number of people tabs and open buttons
+// Open menu buttons
 const open_people_menu = document.querySelectorAll('.open_band_member_menu');
 const people_menu = document.querySelectorAll('.people');
 const button_img = document.querySelectorAll('.open_menu_member_img');
-for (let i = 0; i < open_people_menu.length; i++) {
-    open_people_menu[i].addEventListener('click', () => {
 
+// Add and remove people
+const add_people = document.querySelectorAll('.add_item');
+const remove_people = document.querySelectorAll('.remove_item');
+
+// Arrays to keep track of the amount of people
+let name_arr = [new Array(0), new Array(0)];
+for (let i = 0; i < add_people.length; i++) {
+    add_people[i].addEventListener('click', () => {
+        open_add_menu(open_people_menu[i], people_menu[i], button_img[i]);
+        add_person(people_menu[i], i);
+
+        name_arr[i].forEach((element, index) => element.addEventListener('input', () => {
+            change_name(element, index);
+        }));
+    });
+
+    remove_people[i].addEventListener('click', () => {
+        people_menu[i].removeChild(name_arr[i][name_arr[i].length - 1]);
+        name_arr[i].pop();
     });
 }
 
-let name_arr = new Array(0)
-
-function add_person(parent) {
+function add_person(parent, arr) {
     // Create a new div element
     var new_div = document.createElement('div');
-    new_div.id = `person_${name_arr.length}`;
+    new_div.id = `person_${name_arr[arr].length}`;
 
     // Create elements
     var name_header = document.createElement('h4');
     name_header.className = 'band_member_name';
-    name_header.textContent = `Person ${name_arr.length + 1}`;
+    name_header.textContent = `Person ${name_arr[arr].length + 1}`;
 
     var name_label = document.createTextNode('Name: ');
     var name_input = document.createElement('input');
-    name_input.name = `band_name_${name_arr.length}`; 
+    name_input.name = `band_name_${name_arr[arr].length}`; 
     name_input.type = 'text';
     name_input.className = 'person_name';
 
     var email_label = document.createTextNode('Email: ');
     var email_input = document.createElement('input');
-    email_input.name = `band_email_${name_arr.length}`;
+    email_input.name = `band_email_${name_arr[arr].length}`;
     email_input.type = 'email';
 
     var phone_number_label = document.createTextNode('Phone number: ');
     var phone_number_input = document.createElement('input');
-    phone_number_input.name = `band_phone_${name_arr.length}`;
+    phone_number_input.name = `band_phone_${name_arr[arr].length}`;
     phone_number_input.type = 'text';
 
     // Append elements
     new_div.appendChild(name_header);
-    new_div.appendChild(document.createElement('p'));
     new_div.appendChild(name_label);
     new_div.appendChild(name_input);
     new_div.appendChild(email_label);
@@ -63,14 +78,15 @@ function add_person(parent) {
     new_div.appendChild(phone_number_input);
 
     // Append the new div to the parent div and add it to the array
-    name_arr.push(name_input);
+    name_arr[arr].push(name_input);
     parent.appendChild(new_div);
 }
 
 // Open add / edit people menu
 function open_add_menu(button, menu, img) {
     img.style.rotate = '90deg';
-    
+    menu.style.display = 'block';
+    button.checked = true;
 }
 
 // Change title of persons
@@ -81,5 +97,9 @@ function change_name(e, i) {
         title[i].innerText = e.value;
     } else {
         title[i].innerText = `Person ${i + 1}`;
+    }
+
+    if (e.value.length > window.innerWidth / 50) {
+        title[i].innerText = e.value.slice(0, window.innerWidth / 50) + "...";
     }
 }
