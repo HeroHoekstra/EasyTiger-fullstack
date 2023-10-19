70151045d303e5817838489f3e0ff1c023f36e50
Hero Hoekstra <2172289@talnet.nl>
2023-10-19 14:04:55 +0200

    oops forgot the database

--- database/easy_tiger_patio.sql
+++ database/easy_tiger_patio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 18, 2023 at 07:05 AM
+-- Generation Time: Oct 19, 2023 at 12:03 PM
 -- Server version: 5.7.40
 -- PHP Version: 8.0.26
 
@@ -42,7 +42,10 @@ CREATE TABLE IF NOT EXISTS `band` (
 --
 
 INSERT INTO `band` (`Band_id`, `Naam`, `Genre`, `Herkomst`, `Omschrijving`) VALUES
-('1769842275767399', 'Coole naam', 'Sex music', 'The moon', 'men <3');
+('1776197123819878', 'Eco Terrorism', 'Terrorism pop', 'Urk', 'Murder Kill Death :)'),
+('1776817156889881', 'Scavs', 'Pearl music', 'Garbage Wastes', '*spears you from off screen*'),
+('1778010407184912', 'Realistic Bart', 'Uhhh', 'Springfield', 'Eat short'),
+('1779447080186996', 'Colon three', 'Boy kisser core', 'Den Helder', 'OOOOooOoOoh you like kissing boys');
 
 -- --------------------------------------------------------
 
@@ -60,6 +63,20 @@ CREATE TABLE IF NOT EXISTS `bandleden` (
   PRIMARY KEY (`Lid_id`),
   KEY `fk_Bandleden_Band_idx` (`Band_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `bandleden`
+--
+
+INSERT INTO `bandleden` (`Lid_id`, `Band_id`, `Naam`, `Email`, `Telefoon`) VALUES
+('1776197123821058', '1776197123819878', 'John McBalls', 'mcballs@eco.ter', '06 12345678'),
+('1776197123822056', '1776197123819878', 'Josh', 'josh@eco.ter', '06 90123456'),
+('1776817156896053', '1776817156889881', 'Scav n1', 'givmeporl@scav.com', '06 11231234'),
+('1777364651816065', '1776817156889881', 'Scav n2', 'rockthrower@scav.com', '06 56789910'),
+('1778010407195668', '1778010407184912', 'Bart', 'bart@bart.bart', '06 211820'),
+('1778010407196744', '1778010407184912', 'Bort', 'bort@bort.bort', '06 2151820'),
+('1778010407197028', '1778010407184912', 'Bert', 'bert@bert.bert', '06 251820'),
+('1779447080195128', '1779447080186996', 'John McBalls pt2', 'boy@kisser.com', '06 96969696');
 
 -- --------------------------------------------------------
 
@@ -110,12 +127,22 @@ CREATE TABLE IF NOT EXISTS `bezoekers_events` (
 DROP TABLE IF EXISTS `evenementen`;
 CREATE TABLE IF NOT EXISTS `evenementen` (
   `Event_id` varchar(20) NOT NULL,
+  `Naam` varchar(255) NOT NULL,
   `Datum` date DEFAULT NULL,
   `Starttijd` time DEFAULT NULL,
   `Entreegeld` decimal(10,2) DEFAULT NULL,
   `Drankgeld` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`Event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `evenementen`
+--
+
+INSERT INTO `evenementen` (`Event_id`, `Naam`, `Datum`, `Starttijd`, `Entreegeld`, `Drankgeld`) VALUES
+('1778627131539287', 'Test event', '2023-12-31', '23:59:00', '15.00', NULL),
+('1779910850361213', 'Test event n2', '2024-10-31', '12:00:00', '12.00', NULL),
+('1779911602173817', 'test n3', '2023-12-13', '12:00:00', '69.00', NULL);
 
 -- --------------------------------------------------------
 
@@ -135,6 +162,20 @@ CREATE TABLE IF NOT EXISTS `optredens` (
   KEY `fk_Optredens_Evenementen1_idx` (`Event_id`),
   KEY `fk_Optredens_Band1_idx` (`Band_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `optredens`
+--
+
+INSERT INTO `optredens` (`Optreden_id`, `Event_id`, `Band_id`, `Sets`, `Starttijd`, `Eindtijd`) VALUES
+('1778627131542144', '1778627131539287', '1776197123819878', 5, '00:00:00', '00:30:00'),
+('1778627131547477', '1778627131539287', '1776817156889881', 7, '00:30:00', '01:00:00'),
+('1778627131551590', '1778627131539287', '1778010407184912', 6, '01:30:00', '02:00:00'),
+('1779910850395458', '1779910850361213', '1776817156889881', 7, '13:00:00', '14:00:00'),
+('1779911602181899', '1779911602173817', '1778010407184912', 10, '12:00:00', '13:00:00'),
+('1779911602187544', '1779911602173817', '1776197123819878', 10, '13:00:00', '14:00:00'),
+('1779911602195559', '1779911602173817', '1776817156889881', 10, '14:00:00', '15:00:00'),
+('1779911602199025', '1779911602173817', '1779447080186996', 10, '15:00:00', '16:00:00');
 
 --
 -- Constraints for dumped tables
